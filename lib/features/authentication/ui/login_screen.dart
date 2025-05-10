56b14829a6a95be3d04b7c3c4f8a17a96029e3b1
Jonathan Mereria <jonathan@ikigaidigital.io>
2025-05-10 19:54:41 +0800

    flutter login page

--- lib/features/authentication/ui/login_screen.dart
+++ lib/features/authentication/ui/login_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_login/flutter_login.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:timetracker/constants.dart';
 import 'package:timetracker/features/authentication/services/auth.dart';
-import 'package:timetracker/repository/database.dart';
 
 
 class LoginScreen extends ConsumerStatefulWidget {
@@ -12,91 +14,148 @@ class LoginScreen extends ConsumerStatefulWidget {
 }
 
 class _LoginScreenState extends ConsumerState<LoginScreen> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    BorderRadius inputBorder = BorderRadius.circular(10);
+    return FlutterLogin(
+      title: Constants.appName,
+      logo: const AssetImage('assets/images/logo_topbest.png'),
+      navigateBackAfterRecovery: true,
+      loginAfterSignUp: false,
+      hideForgotPasswordButton: true,
+      hideProvidersTitle: true,
+      userValidator: (value) {
+        if (!value!.contains('@') || !value.endsWith('.ph')) {
+          return "Email must contain '@' and end with '.ph'";
+        }
+        return null;
+      },
+      passwordValidator: (value) {
+        if (value!.isEmpty) {
+          return 'Password is empty';
+        }
+        return null;
+      },
+      onLogin: (loginData) async {
+        if (await UserAuthScope.of(context).signIn(loginData.name, loginData.password)) {
+          return null;
+        }
+        return "Not logged in";
+      },
+      onRecoverPassword: (_)=>null,
+      headerWidget: const IntroWidget(),
+      theme: LoginTheme(
+        primaryColor: Colors.teal,
+        accentColor: Colors.yellow,
+        errorColor: Colors.deepOrange,
+        pageColorLight: Colors.green.shade300,
+        pageColorDark: Colors.green.shade800,
+        logoWidth: 0.80,
+        titleStyle: TextStyle(
+          color: Colors.indigo.shade700,
+          fontFamily: 'Quicksand',
+          fontWeight: FontWeight.w500,
+          letterSpacing: 3,
+          fontSize: 30
+        ),
+        // beforeHeroFontSize: 50,
+        // afterHeroFontSize: 20,
+        bodyStyle: TextStyle(
+          fontStyle: FontStyle.italic,
+          decoration: TextDecoration.underline,
+        ),
+        textFieldStyle: TextStyle(
+          color: Colors.orange,
+          shadows: [Shadow(color: Colors.yellow, blurRadius: 2)],
+        ),
+        buttonStyle: TextStyle(
+          fontWeight: FontWeight.w800,
+          color: Colors.teal,
+        ),
+        cardTheme: CardTheme(
+          color: Colors.white,
+          elevation: 5,
+          margin: EdgeInsets.only(top: 15),
+          shape: ContinuousRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0)),
+        ),
+        inputTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.purple.withOpacity(.1),
+          contentPadding: EdgeInsets.zero,
+          errorStyle: TextStyle(
+            backgroundColor: Colors.orange,
+            color: Colors.white,
+          ),
+          labelStyle: TextStyle(fontSize: 12),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue.shade700, width: 4),
+            borderRadius: inputBorder,
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue.shade400, width: 5),
+            borderRadius: inputBorder,
+          ),
+          errorBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.red.shade700, width: 7),
+            borderRadius: inputBorder,
+          ),
+          focusedErrorBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.red.shade400, width: 8),
+            borderRadius: inputBorder,
+          ),
+          disabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey, width: 5),
+            borderRadius: inputBorder,
+          ),
+        ),
+        buttonTheme: LoginButtonTheme(
+          splashColor: Colors.lightGreen,
+          backgroundColor: Colors.greenAccent,
+          highlightColor: Colors.green,
+          elevation: 9.0,
+          highlightElevation: 6.0,
+          // shape: BeveledRectangleBorder(
+          //   borderRadius: BorderRadius.circular(3),
+          // ),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          // shape: CircleBorder(side: BorderSide(color: Colors.green)),
+          // shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(55.0)),
+        ),
+      ),
+    );
+  }
+}
+
+class IntroWidget extends StatelessWidget {
+  const IntroWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: ListView(
-            children: <Widget>[
-              Container(
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.all(10),
-                  child: const Text(
-                    'Onsite Offline',
-                    style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 30),
-                  )),
-              Container(
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.all(10),
-                  child: const Text(
-                    'Sign in',
-                    style: TextStyle(fontSize: 20),
-                  )),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email Address',
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                child: TextField(
-                  obscureText: true,
-                  controller: passwordController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  //forgot password screen
-                },
-                child: const Text(
-                  'Forgot Password',
-                ),
-              ),
-              Container(
-                  height: 50,
-                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: ElevatedButton(
-                    child: const Text('Login'),
-                    onPressed: () {
-                      debugPrint('login clicked');
-                      UserAuthScope.of(context)
-                          .signIn(emailController.text, passwordController.text);
-                    },
-                  )),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text('Does not have account?'),
-                  TextButton(
-                    child: const Text(
-                      'Sign in',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    onPressed: () {
-                      //signup screen
-                    },
-                  )
-                ],
-
+    return const Column(
+      children: [
+        Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: "Onsite Services",
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
             ],
-          )),
+          ),
+          textAlign: TextAlign.justify,
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(child: Divider()),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("Authenticate"),
+            ),
+            Expanded(child: Divider()),
+          ],
+        ),
+      ],
     );
   }
 }
